fe980c4679ef541a735de58214f4417038966fb3
Thomas BAILLY-SALINS <tbailly-@student.42.fr>
2020-01-22 11:31:08 +0100

    fix header

--- app/src/components/Layout/Header.jsx
+++ app/src/components/Layout/Header.jsx
@@ -1,19 +1,10 @@
-import { useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Button,
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-
-import {
-  updateMessageWithSeverityAction,
-} from '../../store/actions/notifications';
-
-import { Store } from '../../store/Store';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,26 +20,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header() {
   const classes = useStyles();
-  const { state, dispatch } = useContext(Store);
-
-  useEffect(() => {
-    const asyncUseEffect = async () => {
-      setTimeout(() => {
-        updateMessageWithSeverityAction(
-          dispatch,
-          { message: 'A 1st message', severity: 'info' },
-        );
-      }, 1000);
-
-      setTimeout(() => {
-        updateMessageWithSeverityAction(
-          dispatch,
-          { message: 'A 2nd message', severity: 'error' },
-        );
-      }, 10000);
-    };
-    asyncUseEffect();
-  }, []);
 
   return (
     <AppBar position="static">
