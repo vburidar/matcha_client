ddc111e5a2392e3a5bb76cccfed823e92584ecf1
Victor BURIDARD <vburidar@e2r12p1.42.fr>
2020-01-22 11:09:33 +0100

    linting validationAccount

--- app/src/pages/validateAccount.jsx
+++ app/src/pages/validateAccount.jsx
@@ -2,40 +2,39 @@ import React, { useState, useEffect } from 'react';
 import api from '../api';
 
 function HomePage({ url }) {
-  
   const ValidateAccount = () => {
     const [login, setLogin] = useState(url.query.login);
     const [code, setCode] = useState(url.query.code);
-    let [messageValidation, setMsgValidation] = useState('');
-    
+
     useEffect(() => {
       async function validateAccount() {
         if (url.query.login !== undefined && url.query.code !== undefined) {
           setLogin(url.query.login);
           setCode(url.query.code);
-          try { await api.post('auth/accountValidation', {
-            login,
-            code,
-          }); 
-          console.log("validation is successfull");
-        } catch (err) {
-          console.log("validation failed");
-        }
+          try {
+            await api.post('auth/accountValidation', {
+              login,
+              code,
+            });
+            console.log('validation is successfull');
+          } catch (err) {
+            console.log('validation failed');
+          }
         }
       }
       validateAccount();
     }, []);
-    
+
     return (
-      <div><p></p></div>
-    )
-  }
+      <div><p /></div>
+    );
+  };
 
   return (
     <div>
       <h1>Subscription validation</h1>
       <ValidateAccount />
-      <div><p></p></div>
+      <div><p /></div>
     </div>
   );
 }
