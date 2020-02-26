971387c1c29dcc3460380a174dd389732f1b239b
Victor BURIDARD <vburidar@e1r3p9.42.fr>
2020-02-26 16:15:51 +0100

    debug double import in header was making everything crash

--- app/src/components/Layout/Header.jsx
+++ app/src/components/Layout/Header.jsx
@@ -6,7 +6,6 @@ import {
   Button,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import router from 'next/router';
 
 import ButtonListConnected from './Header/ButtonListConnected';
 import ButtonListDisconnected from './Header/ButtonListDisconnected';
